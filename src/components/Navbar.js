ac2d4d5c08843c190f75b9ad7e5ca9fa0b04bdc1
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-26 20:35:43 -0400

    Added content in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom'
+
 //styles
-import styles from '/Navbar.module.css'
+import styles from './Navbar.module.css'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>myMoney</li>
+
+        <li><Link to='/login'>Login</Link></li>
+        <li><Link to='/signup'>Signup</Link></li>
+      </ul>
+    </nav>
   )
 }

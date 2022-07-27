f544afcb64c3b3a94d2293fbeedcec0787a22085
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-26 21:10:35 -0400

    Added form for login with email and password fields

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,6 +3,21 @@ import styles from './Login.module.css'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <form className={styles['login-form']}>
+      <h2>Login</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type='email'
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type='password'
+        />
+      </label>
+      <button className='btn'>Login</button>
+    </form>
   )
 }

acb968f6d8ea0ed1fbc646019ce777f396c469c5
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-26 22:33:35 -0400

    Signup async function try signup user and catch errors

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -4,4 +4,25 @@ import { projectAuth } from '../firebase/config'
 export const useSignup = () => {
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
+
+  const signup = async (email, password, displayName) => {
+    setError(null)
+    setIsPending(true)
+
+    try {
+      // signup user
+      const res = await projectAuth.createUserWithEmailAndPassword(email, password)
+
+      if (!res) {
+        throw new Error('Could not complete signup.')
+      }
+    }
+    catch (err) {
+      console.log(err.message)
+      setError(err.message)
+      setIsPending(false)
+    }
+  }
+
+  return { error, isPending, signup }
 }

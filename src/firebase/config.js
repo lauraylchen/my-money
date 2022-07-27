2353e10cb83bbc03e1fca88fd8b50f4e4b688336
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-26 22:16:28 -0400

    Setup Firebase Authentication service

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app"
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyB6DfJ2J8g8LUDLtGXuXtIuk5_wG-v0Juo",
@@ -15,5 +16,6 @@ firebase.initializeApp(firebaseConfig)
 
 // Init service
 const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
 
-export { projectFirestore }
+export { projectFirestore, projectAuth }

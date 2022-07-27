a23f6e777e043a963d9e8a8b456644ceb01dcabc
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-26 21:48:38 -0400

    Added signup form with 3 input fields : email, password amd display name

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -3,6 +3,27 @@ import styles from './Signup.module.css'
 
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <form className={styles['signup-form']}>
+      <h2>Signup</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type='email'
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type='password'
+        />
+      </label>
+      <label>
+        <span>Display Name:</span>
+        <input
+          type='text'
+        />
+      </label>
+      <button className='btn'>Signup</button>
+    </form>
   )
 }

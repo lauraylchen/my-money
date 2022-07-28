87a098d9a6c181c950c06ae83b89d9976c49a8fa
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-28 00:00:17 -0400

    Cleanup function for useSignup and useLogout hooks to not get error updating state on another page

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { projectAuth } from '../firebase/config'
 import { useAuthContext } from '../hooks/useAuthContext'
 
 export const useSignup = () => {
+  const [isCancelled, setIsCancelled] = useState(false)
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
   const { dispatch } = useAuthContext()
@@ -25,15 +26,23 @@ export const useSignup = () => {
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: res.user })
 
-      setIsPending(false)
-      setError(null)
+      // update state
+      if (!isCancelled) {
+        setIsPending(false)
+        setError(null)
+      }
     }
     catch (err) {
-      console.log(err.message)
-      setError(err.message)
-      setIsPending(false)
+      if (!isCancelled) {
+        setError(err.message)
+        setIsPending(false)
+      }
     }
   }
+
+  useEffect(() => {
+    return () => setIsCancelled(true)
+  }, [])
 
   return { error, isPending, signup }
 }
